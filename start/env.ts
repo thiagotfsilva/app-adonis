8421498464716f2e2eb36206b530fbbb5726bc16
thiagotfsilva <thiagophsilva@hotmail.com>
2024-06-25 08:50:59 -0300

    change envs

--- start/env.ts
+++ start/env.ts
@@ -32,7 +32,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   S3_KEY: Env.schema.string(),
   S3_SECRET: Env.schema.string(),
   S3_REGION: Env.schema.string(),
-
+  S3_BUCKET: Env.schema.string(),
   /*
   |----------------------------------------------------------
   | Mongoose environment variables
